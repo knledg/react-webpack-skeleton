8658e79fc74047e20522979fd2068d6b0fceee4c
Jason Walsh <jason@walsh-web.com>
2016-06-15 08:45:10 -0700

    Remove Emojis

--- src/page/notifications-demo.js
+++ src/page/notifications-demo.js
@@ -27,18 +27,18 @@ export class NotificationsDemo extends React.Component {
         <Row>
           <Col>
             <Panel title='Alert Bar Notifications (Non-closable)'>
-              <Alert type='success'>Yay! You did the thing! <img src='http://www.hey.fr/tools/emoji/ios_emoji_smiling_face_with_open_mouth_and_smiling_eyes.png' height='23px'/></Alert>
-              <Alert type='info'>This alert is informative! <img src='http://emojipedia-us.s3.amazonaws.com/cache/4a/bc/4abc340cf5d893ff4bf6ebc17b29c221.png' height='20px'/></Alert>
-              <Alert type='warning'>Oh no, you're about to do a bad thing! <img src='http://emojipop.net/data/images/emoji_set_21.png' height='22px'/></Alert>
-              <Alert type='danger'>*gasp*! You did a bad thing! <img src='http://www.emoji-quiz-answers.com/wp-content/uploads/2014/11/face_screaming_in_fear-small.png' height='25px'/></Alert>
+              <Alert type='success'>Yay! You did the thing!</Alert>
+              <Alert type='info'>This alert is informative!</Alert>
+              <Alert type='warning'>Oh no, you're about to do a bad thing!</Alert>
+              <Alert type='danger'>*gasp*! You did a bad thing!</Alert>
             </Panel>
           </Col>
           <Col>
             <Panel title='Alert Bar Notifications (Closable)'>
-              <Alert isDismissible={true} type='success'>Yay! You did the thing! <img src='http://www.hey.fr/tools/emoji/ios_emoji_smiling_face_with_open_mouth_and_smiling_eyes.png' height='23px'/></Alert>
-              <Alert isDismissible={true} type='info'>This alert is informative! <img src='http://emojipedia-us.s3.amazonaws.com/cache/4a/bc/4abc340cf5d893ff4bf6ebc17b29c221.png' height='20px'/></Alert>
-              <Alert isDismissible={true} type='warning'>Oh no, you're about to do a bad thing! <img src='http://emojipop.net/data/images/emoji_set_21.png' height='22px'/></Alert>
-              <Alert isDismissible={true} type='danger'>*gasp*! You did a bad thing! <img src='http://www.emoji-quiz-answers.com/wp-content/uploads/2014/11/face_screaming_in_fear-small.png' height='25px'/></Alert>
+              <Alert isDismissible={true} type='success'>Yay! You did the thing!</Alert>
+              <Alert isDismissible={true} type='info'>This alert is informative!</Alert>
+              <Alert isDismissible={true} type='warning'>Oh no, you're about to do a bad thing!</Alert>
+              <Alert isDismissible={true} type='danger'>*gasp*! You did a bad thing!</Alert>
             </Panel>
           </Col>
         </Row>
