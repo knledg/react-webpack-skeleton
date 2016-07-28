9c8d2b460e3c63fff71ac2ce4cfe822c40f13238
Matt Lorey <mattlorey@gmail.com>
2016-07-28 16:29:19 -0700

    Remove karma, add jsdom for testing

---
+++ test/setup.js
@@ -0,0 +1,11 @@
+require('babel-register')();
+
+if (!global.document) { // to make mocha's watch mode work
+  const jsdom = require('jsdom').jsdom;
+  global.document = jsdom('<!doctype html><html><body></body></html>');
+  global.window = document.defaultView;
+  global.navigator = global.window.navigator;
+  global.self = document.defaultView;
+
+  require('whatwg-fetch');
+}
